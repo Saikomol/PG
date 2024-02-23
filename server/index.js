225e3bf5f84082985fbbbf2ec2c54e1053c31d03
saikomol <saikomol@gmail.com>
2024-02-22 23:43:46 -0800

    commit

--- server/index.js
+++ server/index.js
@@ -1,9 +1,33 @@
-// imports here for express and pg
+console.log("hello world");
+const express = require("express");
+const { connect } = require("http2");
+const app = express();
+const path = require("path");
+const pg = require("pg");
 
-// static routes here (you only need these for deployment)
+const client = new pg.Client(
+  process.env.DATABASE_URL || "postgres://localhost/acme_notes_dbclg "
+);
+// console.log(process.env);
 
-// app routes here
+const init = async () => {
+  await client.connect();
+  const SQL = `
+    DROP TABLE IF EXISTS notes;
+    CREATE TABLE notes
+    (
+        id SERIAL PRIMARY KEY,
+        txt VARCHAR(255)
+        starred BOOLEAN DEFAULT FALSE
+        );
+        INSERT INTO notes(txt, starred) VALUES('learn express', false);
+        INSERT INTO notes(txt, starred) VALUES('write SQL queries', true);
+        INSERT INTO notes(txt) VALUES('create routes');
+        `
+  await client.query(SQL);
+  console.log("data seeded");
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+};
 
-// create your init function
-
-// init function invocation
+init();
