75b4567e2ed6ce8145409fb844e625688336596a
saikomol <saikomol@gmail.com>
2024-02-24 02:19:00 -0800

    help

--- server/index.js
+++ server/index.js
@@ -1,33 +1,29 @@
+
+const express = require('express');//1
+const app = express();//1
+const path = require('path');//1
+const pg = require('pg');//1
+const { CLIENT_RENEG_LIMIT } = require('tls');
+
 console.log("hello world");
-const express = require("express");
-const { connect } = require("http2");
-const app = express();
-const path = require("path");
-const pg = require("pg");
 
-const client = new pg.Client(
-  process.env.DATABASE_URL || "postgres://localhost/acme_notes_dbclg "
-);
-// console.log(process.env);
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_notes_db')
 
-const init = async () => {
-  await client.connect();
-  const SQL = `
-    DROP TABLE IF EXISTS notes;
-    CREATE TABLE notes
-    (
-        id SERIAL PRIMARY KEY,
-        txt VARCHAR(255)
-        starred BOOLEAN DEFAULT FALSE
-        );
-        INSERT INTO notes(txt, starred) VALUES('learn express', false);
-        INSERT INTO notes(txt, starred) VALUES('write SQL queries', true);
-        INSERT INTO notes(txt) VALUES('create routes');
-        `
-  await client.query(SQL);
-  console.log("data seeded");
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => console.log(`listening on port ${PORT}`));
-};
+const init = async() => {
+await client.connect();
 
-init();
+const SQL =`
+
+`;
+await client.query(SQL);
+console.log('data seeded');
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+console.log(`listening on port ${port}`)
+})
+
+}
+
+init()
